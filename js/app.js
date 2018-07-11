9ced964663b5f0595a4ea8756368e599bae223c2
Seleah OConnor <seleah.face@gmail.com>
2018-07-11 03:06:47 +0000

    Add instantiation of player and allEnemies

--- js/app.js
+++ js/app.js
@@ -96,8 +96,16 @@ let Player = class {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+let numPossEnemies = [2, 3, 4, 5];
+let numEnemies = numPossEnemies[Math.floor(Math.random) * numPossEnemies.length];
+let allEnemies = [];
 
+for (let i = 0; i < numEnemies; i++) {
+    allEnemies.push(new Enemy());
+}
+
+// Place the player object in a variable called player
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
