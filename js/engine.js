8fb9bb763439de9cf6cb655f01dc9691a23a83b0
Seleah OConnor <seleah.face@gmail.com>
2018-07-11 04:29:06 +0000

    Add debugging line (105). Trying to figure out why the player isn't rendering on the canvas

--- js/engine.js
+++ js/engine.js
@@ -152,7 +152,7 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
+        console.log(player.x);
         player.render();
     }
 
